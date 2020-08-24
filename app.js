5482a71c2d336d47fb67d94baea9ec578ad6f1e5
Javier Sierra <jasierrap@uqvitual.educo>
2020-08-24 18:21:13 -0500

    add environment vars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -23,8 +23,9 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-const secret = "ThisourLittlesecret.";
-userSchema.plugin(encrypt, { secret: secret,  encryptedFields: ['password']  });
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
